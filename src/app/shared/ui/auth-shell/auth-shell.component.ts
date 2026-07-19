e5ba16de0b86379563a76a3f875e4e44fe51734b
AndyBriyit <AndyBriyit12@gmail.com>
2026-07-19 15:55:55 -0500

    feat: :zap: add auth shell

---
+++ src/app/shared/ui/auth-shell/auth-shell.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-shell',
+  standalone: true,
+  templateUrl: './auth-shell.component.html',
+  styleUrl: './auth-shell.component.scss',
+})
+export class AuthShellComponent {}
