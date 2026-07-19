663ed01fe03179b3d74b0510f81ee8d23010780b
AndyBriyit <AndyBriyit12@gmail.com>
2026-07-19 15:58:35 -0500

    feat: :zap: improve app-loyout

---
+++ src/app/core/layout/app-layout/app-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { AuthService } from '../../auth/auth.service';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, SidebarComponent],
+  templateUrl: './app-layout.component.html',
+  styleUrl: './app-layout.component.scss',
+})
+export class AppLayoutComponent {
+  // Inyectamos el auth para que el HTML pueda usar auth.isAuthenticated()
+  protected readonly auth = inject(AuthService);
+}
